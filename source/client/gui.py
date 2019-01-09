8a7279a82ca4c994adf4a80f1b0d5c3eb740a5b4
solar-car <>
2019-01-09 11:49:44 -0800

    more gui

--- source/client/gui.py
+++ source/client/gui.py
@@ -15,8 +15,8 @@ class UserInterface:
         self.pi_connection_status_widget = self.context.findChild(QLabel, "pi_connection_status")
         self.server_connection_status_widget = self.context.findChild(QLabel, "server_connection_status")
         self.module_tree_widget = self.context.findChild(QTreeWidget, "module_tree")
-        print(self.module_tree_widget)
         self.initialize_module_tree(modules)
+        self.update_module_tree(modules)
 
         self.context.show()
 
@@ -26,13 +26,26 @@ class UserInterface:
         self.module_tree_widget.setColumnCount(5)
         self.module_tree_widget.setHeaderLabels(["Sensor", "Value", "Min", "Max", "Status"])
         for module in module_data:
-            tree_item = QTreeWidgetItem([module.name, "debug", "debug", "debug", "debug"])
+            tree_item = QTreeWidgetItem([module.name, "", "", "", ""])
             self.module_tree_widget.addTopLevelItem(tree_item)
             for sensor in module.sensors:
-                sub_tree_item = QTreeWidgetItem([sensor, "a", "a", "a", "a"])
+                sub_tree_item = QTreeWidgetItem([sensor, "debug", "debug", "debug", "debug"])
                 tree_item.addChild(sub_tree_item)
 
     def update_module_tree(self, module_data):
-        pass
+        root = self.module_tree_widget.invisibleRootItem()
+        modules = self.iterate_over_subitems(root)
+        for module in modules:
+            sensors = self.iterate_over_subitems(module)
+            for sensor in sensors:
+                sensor.setData(0, Qt.ItemDataRole.DisplayRole, "test")
+
+    def iterate_over_subitems(self, item):
+        subitems = []
+        for index in range(item.childCount()):
+            subitems.append(item.child(index))
+
+        return subitems
+
 
 
